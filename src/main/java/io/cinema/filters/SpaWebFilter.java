30b29c8097533f43f07149f3ed674b869fa19074
Santiago Gonzalez Diaz <rstdtas@proton.me>
2026-04-20 13:38:17 -0400

    feat: Add Unit tests

--- src/main/java/io/cinema/filters/SpaWebFilter.java
+++ src/main/java/io/cinema/filters/SpaWebFilter.java
@@ -23,7 +23,13 @@ public class SpaWebFilter implements WebFilter {
                         !path.startsWith("/v3/api-docs") &&
                         path.matches("[^\\\\.]*")
         ) {
-            return chain.filter(exchange.mutate().request(exchange.getRequest().mutate().path("/index.html").build()).build());
+            return chain.filter(
+                    exchange.mutate()
+                            .request(
+                                    exchange
+                                            .getRequest().mutate()
+                                            .path("/index.html").build()
+                            ).build());
         }
 
         return chain.filter(exchange);
